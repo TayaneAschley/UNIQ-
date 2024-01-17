79d4975e15e6c5d608ec457263cc981a9840e928
tayane <asch.tayane@gmail.com>
2024-01-17 19:43:54 -0300

    adiciona nova rota para login

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const routerRegister = require("./routers/register");
+const routerLogin = require("./routers/login");
 
 app.use(express.json());
 app.use(routerRegister);
+app.use(routerLogin);
 
 app.listen(port, () => {
   console.log(`servidor aberto na porta ${port}`);
