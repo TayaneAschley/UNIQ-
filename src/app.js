4c8dba6fef80772a1a2cc2e62806d5be048478c5
tayane <asch.tayane@gmail.com>
2024-01-18 16:11:23 -0300

    adiciona uso cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
 const port = process.env.PORT || 3000;
+const URL = process.env.URL;
+const corsOptions = { origin: URL };
+
 const routerRegister = require("./routers/register");
 const routerLogin = require("./routers/login");
 
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use(routerRegister);
 app.use(routerLogin);
 
